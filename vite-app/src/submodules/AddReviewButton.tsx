4911f81d84044b588e4b90fae94764955a4a4321
Minhyeong Kim <ytzka14@snu.ac.kr>
2023-10-03 00:15:41 +0900

    fix jsx creation

--- vite-app/src/submodules/AddReviewButton.tsx
+++ vite-app/src/submodules/AddReviewButton.tsx
@@ -15,11 +15,10 @@ const getNewId = createIncrementer();
 
 function AddReviewButton() {	
 	const [isModalVisible, setIsModalVisible] = useState(false);
-	const [items, setItems] = useState<[number, JSX.Element][]>(InitialData());
+	const [items, setItems] = useState<[number, string, string, number, string][]>(InitialData());
 
 	const openModal = (e: React.MouseEvent) => {
 		e.preventDefault();
-		console.log("opened Modal");
 		setIsModalVisible(true);
 		console.log(isModalVisible);
 	};
@@ -29,30 +28,33 @@ function AddReviewButton() {
 		setIsModalVisible(false);
 	};
 
-	const addReview = (snackName: string, snackImage: string, snackRate: number, snackText: string) => {
-		const newItem = (
+	function makeJSX(sName: string, sImage: string, sRate: number, sText: string) {
+		return (
 			<>
 				<div className="imageBox">
-					<img src={snackImage} alt={snackName} className="snackImage"/>
+					<img src={sImage} alt={sName} className="snackImage"/>
 				</div>
 				<div className="textBox">
-					<span className="snackNameText">{snackName}</span>
+					<span className="snackNameText">{sName}</span>
 					<span className="greyText"> / </span>
-					<span className="rateSpan">★{snackRate.toFixed(1)}</span>
-					<p>{snackText}</p>
+					<span className="rateSpan">★{sRate.toFixed(1)}</span>
+					<p>{sText}</p>
 				</div>
 			</>
 		)
-		const newID = getNewId();
+	}
 
-		setItems([...items, [newID, newItem]]);
+	const addReview = (snackName: string, snackImage: string, snackRate: number, snackText: string) => {
+		const newID = getNewId();
+		
+		setItems([...items, [newID, snackName, snackImage, snackRate, snackText]]);
 		setIsModalVisible(false);
 	}
 
 	return (
 		<>
 			<ul className="review-list" data-testid="review-list">
-				{items.map(item => <div className="block" key={item[0]}>{item[1]}</div>)}	
+				{items.map(item => <div className="block" key={item[0]}>{makeJSX(item[1], item[2], item[3], item[4])}</div>)}	
 			</ul>
 			{isModalVisible && (
 				<div className="overlay">
--- vite-app/src/submodules/InitialData.tsx
+++ vite-app/src/submodules/InitialData.tsx
@@ -1,33 +1,9 @@
 import '../App.css'
 
 const InitialData = () => {
-	const neoguri = (
-		<>
-			<div className="imageBox">
-				<img src="https://image.nongshim.com/non/pro/1598000561204.jpg" alt="얼큰한 너구리" className="snackImage"/>
-			</div>
-			<div className="textBox">
-				<span className="snackNameText">얼큰한 너구리</span>
-				<span className="greyText"> / </span>
-				<span className="rateSpan">★5.0</span>
-				<p>오동통통한 면과 동봉된 다시마가 맛있습니다</p>
-			</div>
-		</>
-	);
-	const mccol = (
-		<>
-			<div className="imageBox">
-				<img src="https://img.danawa.com/prod_img/500000/350/174/img/3174350_1.jpg?shrink=330:*&_v=20220421104604" alt="맥콜" className="snackImage"/>
-			</div>
-			<div className="textBox">
-				<span className="snackNameText">맥콜</span>
-				<span className="greyText"> / </span>
-				<span className="rateSpan">★4.0</span>
-				<p>보리차와 콜라가 반반 섞인 듯한 맛이 독특합니다.</p>
-			</div>
-		</>
-	);	
-	const data: [number, JSX.Element][] = [[101, neoguri], [103, mccol]];
+	const data: [number, string, string, number, string][] =
+		[[101, "얼큰한 너구리", "https://image.nongshim.com/non/pro/1598000561204.jpg", 5, "오동통통한 면과 동봉된 다시마가 맛있습니다"],
+		[103, "맥콜", "https://img.danawa.com/prod_img/500000/350/174/img/3174350_1.jpg?shrink=330:*&_v=20220421104604", 4, "보리차와 콜라가 반반 섞인 듯한 맛이 독특합니다."]];
 	return data;
 };
 
